560297579eebb5d51fa80358c82e2e06d85c77ad
dronlee <dronlee@list.ru>
2019-11-27 02:15:53 +0300

    Дополнил тест на проверку нормализации выражения, чтобы учитывался случай со слагаемым без переменной.

--- SumInterpreter/UnitTestProject/ExpressionTests.cs
+++ SumInterpreter/UnitTestProject/ExpressionTests.cs
@@ -12,13 +12,13 @@ namespace UnitTestProject
         /// <param name="firstTermMultiplier">Множитель первого слагаемого.</param>
         /// <param name="expected">Ожидаемое нормализованное выражение.</param>
         [Theory]
-        [InlineData(1, "-y^2 + 4.5xy = 0")]
-        [InlineData(2, "x^2 - y^2 + 4.5xy = 0")]
-        [InlineData(3, "2x^2 - y^2 + 4.5xy = 0")]
-        [InlineData(-1, "-2x^2 - y^2 + 4.5xy = 0")]
+        [InlineData(1, "-y^2 + 4.5xy - 2 = 0")]
+        [InlineData(2, "x^2 - y^2 + 4.5xy - 2 = 0")]
+        [InlineData(3, "2x^2 - y^2 + 4.5xy - 2 = 0")]
+        [InlineData(-1, "-2x^2 - y^2 + 4.5xy - 2 = 0")]
         public void Normalization(decimal firstTermMultiplier, string expected)
         {
-            // Составляем выражение: [firstTermMultiplier]x^2 + 3.5xy + y - x^2 = y^2 - xy + y
+            // Составляем выражение: [firstTermMultiplier]x^2 + 3.5xy + y - x^2 = y^2 + 2 - xy + y
             var expression = new Expression
             {
                 LeftPart = new[]
@@ -31,6 +31,7 @@ namespace UnitTestProject
                 RightPart = new[]
                 {
                     new TermStruct { Multiplier = 1, Power = 2, Variable = "y" },
+                    new TermStruct { Multiplier = 2, Power = 1 },
                     new TermStruct { Multiplier = -1, Power = 1, Variable ="xy" },
                     new TermStruct { Multiplier = 1, Power = 1, Variable = "y" }
                 }

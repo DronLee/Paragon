30465d60c0899bd60b589b86ae14f5df53eae8c8
dronlee <dronlee@list.ru>
2019-11-26 09:26:07 +0300

    Добавил интерфейс интерпреатора слагаемого и класс, реализующий его. Но класс пока на заполнял.

---
+++ SumInterpreter/SumInterpreter/ITermInterpreter.cs
@@ -0,0 +1,20 @@
+﻿namespace SumInterpreter
+{
+    /// <summary>
+    /// Интерфейс интерпретатора слагаемого.
+    /// </summary>
+    public interface ITermInterpreter
+    {
+        /// <summary>
+        /// Текст регулярного выражения, для определения слагаемого.
+        /// </summary>
+        string RegexText { get; }
+
+        /// <summary>
+        /// Интерпретация текста слагаемого в структуру.
+        /// </summary>
+        /// <param name="termText">Текст слагаемого.</param>
+        /// <returns>Полученная из текста структура.</returns>
+        Term Interpret(string termText);
+    }
+}
---
+++ SumInterpreter/SumInterpreter/TermInterpreter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumInterpreter
+{
+    /// <summary>
+    /// Интерпретатор слагаемого.
+    /// </summary>
+    public class TermInterpreter : ITermInterpreter
+    {
+        /// <summary>
+        /// Текст регулярного выражения, для определения слагаемого.
+        /// </summary>
+        public string RegexText => throw new NotImplementedException();
+
+        /// <summary>
+        /// Интерпретация текста слагаемого в структуру.
+        /// </summary>
+        /// <param name="termText">Текст слагаемого.</param>
+        /// <returns>Полученная из текста структура.</returns>
+        public Term Interpret(string termText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

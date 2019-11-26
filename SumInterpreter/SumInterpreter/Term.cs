d61694187ce9547cf20976893ec69fad83c18218
dronlee <dronlee@list.ru>
2019-11-26 09:17:22 +0300

    Добавил структуру слагаемого.

---
+++ SumInterpreter/SumInterpreter/Term.cs
@@ -0,0 +1,23 @@
+﻿namespace SumInterpreter
+{
+    /// <summary>
+    /// Структура слагаемого.
+    /// </summary>
+    public struct Term
+    {
+        /// <summary>
+        /// Множитель.
+        /// </summary>
+        public decimal Multiplier;
+
+        /// <summary>
+        /// Переменная или переменные.
+        /// </summary>
+        public string Variable;
+
+        /// <summary>
+        /// Степень.
+        /// </summary>
+        public int Power;
+    }
+}

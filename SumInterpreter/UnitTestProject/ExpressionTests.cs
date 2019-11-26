da02812ff871e897b30228edaf431188c8da3006
dronlee <dronlee@list.ru>
2019-11-27 00:00:23 +0300

    Доработал тест на проверку нормализации выражения, чтобы проверяось несколько вариантов.

--- SumInterpreter/UnitTestProject/ExpressionTests.cs
+++ SumInterpreter/UnitTestProject/ExpressionTests.cs
@@ -7,31 +7,38 @@ namespace UnitTestProject
     public class ExpressionTests
     {
         /// <summary>
-        /// Проверка норрмализации выражения.
+        /// Проверка нормализации выражения.
         /// </summary>
-        [Fact]
-        public void Normalization()
+        /// <param name="firstTermMultiplier">Множитель первого слагаемого.</param>
+        /// <param name="expected">Ожидаемое нормализованное выражение.</param>
+        [Theory]
+        [InlineData(1, "-y^2 + 4.5xy = 0")]
+        [InlineData(2, "x^2 - y^2 + 4.5xy = 0")]
+        [InlineData(3, "2x^2 - y^2 + 4.5xy = 0")]
+        [InlineData(-1, "-2x^2 - y^2 + 4.5xy = 0")]
+        public void Normalization(decimal firstTermMultiplier, string expected)
         {
-            // Составляем выражение: x^2 + 3.5xy + y = y^2 - xy + y
+            // Составляем выражение: [firstTermMultiplier]x^2 + 3.5xy + y - x^2 = y^2 - xy + y
             var expression = new Expression
             {
                 LeftPart = new[]
                 {
-                    new TermStruct { Multiplier = 1, Power = 2, Variable = "x"  },
-                    new TermStruct{  Multiplier = (decimal)3.5, Power = 1,  Variable = "xy" },
-                    new TermStruct{ Multiplier = 1, Power = 1, Variable = "y" }
+                    new TermStruct { Multiplier = firstTermMultiplier, Power = 2, Variable = "x" },
+                    new TermStruct { Multiplier = (decimal)3.5, Power = 1,  Variable = "xy" },
+                    new TermStruct { Multiplier = 1, Power = 1, Variable = "y" },
+                    new TermStruct { Multiplier = -1, Power = 2, Variable = "x" },
                 },
                 RightPart = new[]
                 {
                     new TermStruct { Multiplier = 1, Power = 2, Variable = "y" },
-                    new TermStruct{ Multiplier = -1, Power = 1, Variable ="xy" },
-                    new TermStruct{ Multiplier = 1, Power = 1, Variable = "y" }
+                    new TermStruct { Multiplier = -1, Power = 1, Variable ="xy" },
+                    new TermStruct { Multiplier = 1, Power = 1, Variable = "y" }
                 }
             };
 
             var result = expression.Normalization();
 
-            Assert.Equal("x^2 - y^2 + 4.5xy = 0", result);
+            Assert.Equal(expected, result);
         }
     }
 }

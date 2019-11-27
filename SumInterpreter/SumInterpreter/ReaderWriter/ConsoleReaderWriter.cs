104a5165b50e0c26d997b8616f7289ed3711a3c1
dronlee <dronlee@list.ru>
2019-11-27 04:15:24 +0300

    Добавил считыватель из консоли и писатель в консоль.

---
+++ SumInterpreter/SumInterpreter/ReaderWriter/ConsoleReaderWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumInterpreter.ReaderWriter
+{
+    /// <summary>
+    /// Считыватель из консоли и писатель в консоль.
+    /// </summary>
+    public class ConsoleReaderWriter : IReaderWriter
+    {
+        /// <summary>
+        /// Получение входных выражений.
+        /// </summary>
+        /// <returns>Выражения.</returns>
+        public IEnumerable<string> GetInputExpressions()
+        {
+            while(true)
+            {
+                Console.WriteLine("Введите выражение: ");
+                yield return Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Записать результат.
+        /// </summary>
+        /// <param name="resultExpression">Выражение результата для записи.</param>
+        public void WriteResult(string resultExpression)
+        {
+            Console.WriteLine(resultExpression);
+        }
+    }
+}

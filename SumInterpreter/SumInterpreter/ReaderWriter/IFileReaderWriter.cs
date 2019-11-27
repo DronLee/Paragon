3266b2775a8bd9d228cc45b75d5b21a465686897
dronlee <dronlee@list.ru>
2019-11-27 04:08:33 +0300

    Добавил интерфейсы считывателей-писателей.

---
+++ SumInterpreter/SumInterpreter/ReaderWriter/IFileReaderWriter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SumInterpreter.ReaderWriter
+{
+    /// <summary>
+    /// Интерфейс для считывателя из файла и писателя в файл.
+    /// </summary>
+    interface IFileReaderWriter : IReaderWriter, IDisposable { }
+}
---
+++ SumInterpreter/SumInterpreter/ReaderWriter/IReaderWriter.cs
@@ -0,0 +1,7 @@
+﻿namespace SumInterpreter.ReaderWriter
+{
+    /// <summary>
+    /// Единный интерфейс для считывателя входных данных и писателя результата.
+    /// </summary>
+    public interface IReaderWriter : IInputReader, IResultWriter { }
+}

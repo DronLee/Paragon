91baeaedb44d64f76528c18149b81567e139200e
dronlee <dronlee@list.ru>
2019-11-27 04:05:01 +0300

    Добавил интерфейс для чтения входных данных и интерфейс для записи результата.

---
+++ SumInterpreter/SumInterpreter/ReaderWriter/IInputReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SumInterpreter.ReaderWriter
+{
+    /// <summary>
+    /// Интерфейс считывателя входных данных.
+    /// </summary>
+    public interface IInputReader
+    {
+        /// <summary>
+        /// Получение входных выражений.
+        /// </summary>
+        /// <returns>Выражения.</returns>
+        IEnumerable<string> GetInputExpressions();
+    }
+}
---
+++ SumInterpreter/SumInterpreter/ReaderWriter/IResultWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace SumInterpreter.ReaderWriter
+{
+    /// <summary>
+    /// Интерфейс писателя результата.
+    /// </summary>
+    public interface IResultWriter
+    {
+        /// <summary>
+        /// Записать результат.
+        /// </summary>
+        /// <param name="resultExpression">Выражение результата для записи.</param>
+        void WriteResult(string resultExpression);
+    }
+}
